460dafda04064802272598ec34eced92702cc099
Kwiatuh <46192203+kwiats@users.noreply.github.com>
2022-02-23 15:17:56 +0100

    Added critical distance to target
    Dodana nowa zmienna distanceOfTarget

--- Assets/Scripts/SensorScript.cs
+++ Assets/Scripts/SensorScript.cs
@@ -9,7 +9,7 @@ public class SensorScript : MonoBehaviour
     public float fullAngle = 360f;
     public float distanceOfObject = 5f;
     public int numberOfGizmos = 6;
-    public float distanceOfTarget = 15f;
+    public float distanceOfTarget = 5f;
 
     public RawImage sensorUP;
     public RawImage sensorDOWN;
@@ -43,48 +43,47 @@ public class SensorScript : MonoBehaviour
             }
 
         }
-
-        if (distances[0] < 5 && distances[0] > 1)
+        //ODLEGLOSC KRYTYCZNA
+        if (distances[0] < distanceOfTarget && distances[0] > 1)
         {
             sensorUP.enabled = true;
-            sensorUP.color = Color.red;
             Debug.Log("Czujnik UP uruchomiony! Dystans do obiektu: " + distances[0]);
         }
-        else if (distances[1] < 5 && distances[1] > 1)
+        else if (distances[1] < distanceOfTarget && distances[1] > 1)
         {
             sensorLEFT.enabled = true;
             Debug.Log("Czujnik RIGHT uruchomiony! Dystans do obiektu: " + distances[1]);
         }
-        else if ((distances[2] < 5 && distances[2] > 1) || (distances[3] < 5 && distances[3] > 1))
+        else if ((distances[2] < distanceOfTarget && distances[2] > 1) || (distances[3] < distanceOfTarget && distances[3] > 1))
         {
             sensorDOWN.enabled = true;
             Debug.Log("Czujnik DOWN uruchomiony! Dystans do obiektu: " + distances[2]);
 
         }
-        else if (distances[4] < 5 && distances[4] > 1)
+        else if (distances[4] < distanceOfTarget && distances[4] > 1)
         {
             sensorRIGHT.enabled = true;
             Debug.Log("Czujnik LEFT uruchomiony! Dystans do obiektu: " + distances[4]);
         }
-        else if ((distances[0] < 5 && distances[0] > 1) && (distances[1] < 5 && distances[1] > 1))
+        else if ((distances[0] < distanceOfTarget && distances[0] > 1) && (distances[1] < distanceOfTarget && distances[1] > 1))
         {
             sensorUP.enabled = true;
             sensorLEFT.enabled = true;
             Debug.Log("Czujnik UP and RIGHT  uruchomiony! Dystans do obiektu: " + distances[0]);
         }
-        else if ((distances[0] < 5 && distances[0] > 1) && (distances[4] < 5 && distances[4] > 1))
+        else if ((distances[0] < distanceOfTarget && distances[0] > 1) && (distances[4] < distanceOfTarget && distances[4] > 1))
         {
             sensorUP.enabled = true;
             sensorRIGHT.enabled = true;
             Debug.Log("Czujnik UP and LEFT  uruchomiony! Dystans do obiektu: " + distances[0]);
         }
-        else if ((distances[1] < 5 && distances[1] > 1) && (distances[2] < 5 && distances[2] > 1))
+        else if ((distances[1] < distanceOfTarget && distances[1] > 1) && (distances[2] < distanceOfTarget && distances[2] > 1))
         {
             sensorLEFT.enabled = true;
             sensorDOWN.enabled = true;
             Debug.Log("Czujnik DOWN and RIGHT  uruchomiony! Dystans do obiektu: " + distances[1]);
         }
-        else if ((distances[3] < 5 && distances[3] > 1) && (distances[4] < 5 && distances[4] > 1))
+        else if ((distances[3] < distanceOfTarget && distances[3] > 1) && (distances[4] < distanceOfTarget && distances[4] > 1))
         {
             sensorDOWN.enabled = true;
             sensorRIGHT.enabled = true;
